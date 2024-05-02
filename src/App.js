2c43a23ea5a51f9cca4672faee0038685364a40d
Beti Frim <betifrim@Betis-MBP.home>
2024-05-02 18:04:10 +0200

    animacija, scrol

--- src/App.js
+++ src/App.js
@@ -1,8 +1,7 @@
-import React, { useCallback, useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import bgImage from "./assets/bg.png";
 import PixelmossImages from "./components/PixelmossImages";
 import Layout from "./components/Layout";
-import ImpulzImages from "./components/ImpulzImages";
 import Hello from "./components/Hello";
 import "./App.css";
 
@@ -17,55 +16,12 @@ const backgroundStyle = {
 };
 
 function App() {
-  const scrollableRefs = useRef([]);
-
-  const [positions, setPositions] = useState([
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-    { x: 0, y: 0 },
-  ]);
-
-  useEffect(() => {
-    const handleScroll = (index) => () => {
-      setPositions((prevPositions) => {
-        const newPositions = [...prevPositions];
-        newPositions[index] = {
-          x: Math.random() * window.innerWidth - 450,
-          y: Math.random() * window.innerHeight - 450,
-        };
-        return newPositions;
-      });
-    };
-
-    scrollableRefs.current.forEach((ref, index) => {
-      if (ref) {
-        ref.addEventListener("scroll", handleScroll(index));
-      }
-    });
-
-    return () => {
-      scrollableRefs.current.forEach((ref, index) => {
-        if (ref) {
-          ref.removeEventListener("scroll", handleScroll(index));
-        }
-      });
-    };
-  }, []);
-
-  const setScrollableRef = useCallback(
-    (index) => (ref) => {
-      scrollableRefs.current[index] = ref;
-    },
-    []
-  );
-
   const [displayHello, setDisplayHello] = useState(true);
 
   return (
     <div style={backgroundStyle} className="custom-cursor"> 
-      <Layout setScrollableRef={setScrollableRef} />
+      <Layout />
       <PixelmossImages />
-      {/* <ImpulzImages positions={positions} /> */}
       {displayHello && <Hello onSelectYes={() => setDisplayHello(false)} />}
     </div>
   );
